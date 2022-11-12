d3e16de203ef926675e35d83bd8b98f5fb52ba06
Rajat-soni-vst-au4 <Rajatsoni.rs66@gmail.com>
2022-11-12 20:24:49 +0530

    styling done

--- index.js
+++ index.js
@@ -1,34 +1,39 @@
-const quote_box = document.getElementById("quote_box")
+
 const text = document.getElementById("text")
 const author = document.getElementById("author")
-const new_quote = document.getElementById("new_quote")
-const tweet = document.getElementById("tweet_quote")
 
- function quote() {
+const tweet = document.getElementById("tweet-quote")
+
+
     try {
         fetch("https://type.fit/api/quotes")
             .then(function(response) {
                 return response.json();
             })
             .then(function(data) {
-                console.log("data", data);
+                // console.log("data", data);
                 function getRandomQuote () {
                     var randomNumber = Math.floor(Math.random() * (data.length));
                     var randomQuote = data[randomNumber];
-                   
-                    // let text = document.createElement("p")
+                
                     text.innerText = randomQuote.text;
-                    author.innerText = randomQuote.author;
-                    quote_box.append(text,author);
-
+                    author.innerText = "-"+randomQuote.author;
+                    
+                    var newbutton = document.createElement("button")
+                    button.setattribute("id", "new-quote")
+                    button.text = "textContent"
+                    document.getElementById("quote-box").append(text,author,newbutton);
+                
                 }	
-                getRandomQuote() 
+                document.getElementById("new-quote").addEventListener("click", getRandomQuote())
         });
     } catch (error) {
         console.log("error",error);
     }
- }
- quote();
+
+
+ 
+
 
 
 
